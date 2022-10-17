23e672cb79f079a94c92c8b0731d72e4199ee607
Jarvis-wang <46759531+Jarvis-wang@users.noreply.github.com>
2022-10-16 22:17:24 -0400

    a5 JavaScript sp22

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -3,7 +3,7 @@ import WhoToFollowList from "../WhoToFollowList/index.js";
 import HomeScreen from "./HomeScreen.js"
 function exploreComponent() {
     $('#wd-explore').append(`
-       <h2>Explore</h2>
+       <h2>Home</h2>
   <div class="row mt-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
    <!--<h3>Navigation Sidebar</h3>-->
